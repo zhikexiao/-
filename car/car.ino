d1ff8e99bd1fec605ce70607743743a555c22603
zhikexiao <52353414+zhikexiao@users.noreply.github.com>
2019-07-05 11:24:24 +0800

    Add files via upload

--- car/car.ino
+++ car/car.ino
@@ -7,14 +7,14 @@ void setup()
   pinMode(2, OUTPUT);
   pinMode(3, OUTPUT);
   pinMode(4, OUTPUT);
-  pinMode(1, OUTPUT);
+  pinMode(7, OUTPUT);
   Serial.begin(9600);
 }
-int income=0;
+int a=0;
 
 void forward()
 { 
-  digitalWrite(1, HIGH);
+  digitalWrite(7, HIGH);
   digitalWrite(2, LOW);
   digitalWrite(3, HIGH);
   digitalWrite(4, LOW);
@@ -27,7 +27,7 @@ void forward()
 
 void backward()
 {
-  digitalWrite(1, LOW);
+  digitalWrite(7, LOW);
   digitalWrite(2, HIGH);
   digitalWrite(3, LOW);
   digitalWrite(4, HIGH); 
@@ -40,7 +40,7 @@ void backward()
 
 void left()
 {
-  digitalWrite(1, LOW);
+  digitalWrite(7, LOW);
   digitalWrite(2, LOW);
   digitalWrite(3, HIGH);
   digitalWrite(4, HIGH);
@@ -53,7 +53,7 @@ void left()
 
 void right()
 { 
-  digitalWrite(1, HIGH);
+  digitalWrite(7, HIGH);
   digitalWrite(2, HIGH);
   digitalWrite(3, LOW);
   digitalWrite(4, LOW);
@@ -66,7 +66,7 @@ void right()
 
 void stop()
 {  
-  digitalWrite(1, LOW);
+  digitalWrite(7, LOW);
   digitalWrite(2, LOW);
   digitalWrite(3, LOW);
   digitalWrite(4, LOW);
@@ -81,8 +81,8 @@ void loop()
 {
   if(Serial.available()>0)
     {
-      income=Serial.read();
-        switch(income)
+      a=Serial.read();
+        switch(a)
         {
           case 'f':
               forward();
